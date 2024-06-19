8c14535564efe8b9c2c2697008defdd5347b75e6
Diya455 <diyu455@gmail.com>
2024-06-19 17:46:20 +0000

    new change

--- 01_basics/02_Datatypes.js
+++ 01_basics/02_Datatypes.js
@@ -4,3 +4,21 @@
 
 console.log(3+3)
 console.log("Diya")
+
+let name="Diya"
+let age=19
+let isLoggedIn=true
+
+//number => (2^53)
+//bigint 
+//string => ""    i.e use it either in double quotes or not 
+//boolean => ture or false only
+//null => standalone value i.e empty value
+//undefined => value isnt assigned but its not empyt
+//symbol => for uniqueness of anything
+
+
+//object
+
+console.log(typeof undefined);  //output appears to be undefined
+console.log(typeof null);       //output appears to be object
