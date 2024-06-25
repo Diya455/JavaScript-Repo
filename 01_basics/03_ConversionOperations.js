e392a92d24510c113bc7f1d060d81bedc004e8d6
Diya455 <diyu455@gmail.com>
2024-06-25 09:00:29 +0000

    complete for part-3

--- 01_basics/03_ConversionOperations.js
+++ 01_basics/03_ConversionOperations.js
@@ -1,11 +1,11 @@
-let score = false
+// let score = false
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 
 //when converting a number=33 it easily converts 
@@ -14,22 +14,22 @@ console.log(valueInNumber)
 //"33abc"=NaN i.e Not a Number
 //true=1; false=0
 
-let isLoggedIn = "Diya"
-let booleanLoggedIn = Boolean(isLoggedIn)
-console.log(booleanLoggedIn)
+// let isLoggedIn = "Diya"
+// let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn)
 
 //when boolean value is 1 it gives output as true
 //when boolean value is 0 it gives output as false
 //when entered "" output came out to be false
 //when entered "Diya" output came out to be true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber)
 
 
-let value = 3
-let negValue = -value
+// let value = 3
+// let negValue = -value
 // console.log(negValue)
 // console.log(3+3);
 // console.log(3-3);
@@ -37,7 +37,26 @@ let negValue = -value
 // console.log(2/3);
 // console.log(2%3);
 
-let str1 = "Diya"
-let str2 = " Shah"
-let str3 = str1 +str2
-console.log(str3);
+// let str1 = "Diya"
+// let str2 = " Shah"
+// let str3 = str1 +str2
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "1")
+// console.log((3+4)*5%6)
+
+// console.log(+true)
+// console.log(+"")
+
+let num1,num2,num3
+num1=num2=num3=2+2
+console.log(num1);
+
+let gameCounter=100
+++gameCounter   //or else if we write gameCounter++ returns the same output and no change.
+                // in prefix the operator increments after and in postfix the operator increments before.
+console.log(gameCounter);
+
