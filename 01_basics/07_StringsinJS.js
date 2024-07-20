48795dbb490c8a31f9c9c56de133df9316bb0d3e
Diya455 <diyu455@gmail.com>
2024-07-20 10:25:08 +0000

    creation of new file

--- 01_basics/07_StringsinJS.js
+++ 01_basics/07_StringsinJS.js
@@ -4,10 +4,25 @@ const repoCount=50
 
 console.log(`Hello from ${name} to all ${repoCount}`);  //better way to concat two strings.
 
-const gameName=new String('diya shah');
+//string methods:-
+const gameName=new String('diyashah');
 console.log(gameName[4]);
 console.log(gameName.__proto__);
 console.log(gameName.length);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(7));
-console.log(gameName.indexOf(s));
+console.log(gameName.indexOf('s'));
+
+const newString=gameName.substring(0,5);   //here the end number is 5 but we get the output only upto 4th number.
+console.log(newString);
+
+const anotherString=gameName.slice(-8,4);   //here the end number is 5 but we get the output only upto 4th number.
+console.log(anotherString);
+
+const newStringOne="   DiyaShah   ";
+console.log(newStringOne.trim());     //simply eliminates the extra space that is not required.
+
+const url="https://diyashah.com%20shah";
+console.log(url.replace("diyashah","onlyshahs"));
+console.log(url.includes('1234'));
+console.log(url.includes('://'));           //checks wether the value is in it or not.
