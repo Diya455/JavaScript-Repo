690962703b52d0aad22bb8164b0d215f828c05b8
Diya455 <diyu455@gmail.com>
2024-07-20 06:53:07 +0000

    string file change

--- 01_basics/07_StringsinJS.js
+++ 01_basics/07_StringsinJS.js
@@ -1,3 +1,13 @@
 const name="Diya"
 const repoCount=50
-console.log(name + repoCount + " Value");
+// console.log(name + repoCount + " Value");    //this is old method and not productive.
+
+console.log(`Hello from ${name} to all ${repoCount}`);  //better way to concat two strings.
+
+const gameName=new String('diya shah');
+console.log(gameName[4]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(7));
+console.log(gameName.indexOf(s));
