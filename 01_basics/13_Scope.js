cb958523b02fd8645d536025aa3e9a0d903815cb
Diya455 <diyu455@gmail.com>
2024-07-25 12:35:31 +0000

    new file

---
+++ 01_basics/13_Scope.js
@@ -0,0 +1,12 @@
+function one()
+{
+    const userName="Diya"
+    function two()
+    {
+        const websiteName="youtube"
+        console.log(userName);
+    }
+    // console.log(websiteName);
+    two()
+}
+one()
